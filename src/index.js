dd33ea603f72e08c8293b875c3bdc42c7ef1687d
danricardo88 <daniel.ricardo88@outlook.com>
2022-11-09 02:29:56 -0300

    disgrama

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const HTTP_ERRO = 404;
 const PORT = '3000';
+const NOT_VALIDATED = 400;
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -37,9 +38,24 @@ app.get('/talker/:id', async (_request, response) => {
     return response.status(HTTP_OK_STATUS).json(idTlkr);
 });
 
-app.post('/login', async (_request, response) => {
-const tokenAleatorio = Math.random().toString().slice(-16);
-response.status(HTTP_OK_STATUS).json({ token: tokenAleatorio });
-});
+function handleLogin(_request, response, prox) {
+  const { email, password } = _request.body;
+  if (email === undefined) {
+    return response.status(NOT_VALIDATED)
+    .json({ message: 'O campo "email" é obrigatório' });
+  } if (email !== 'emailtaerradocabra@hotmail.com') {
+    return response.status(NOT_VALIDATED)
+    .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  } if (password === undefined) {
+    return response.status(NOT_VALIDATED).json({ message: 'O campo "password" é obrigatório' });
+  } if (password.length < 6) {
+    return response.status(NOT_VALIDATED)
+    .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  } prox();
+}
 
+app.post('/login', handleLogin, async (_request, response) => {
+  const tokenAleatorio = Math.random().toString().slice(-16);
+  response.status(HTTP_OK_STATUS).json({ token: tokenAleatorio });
+  });
 module.exports = app;
